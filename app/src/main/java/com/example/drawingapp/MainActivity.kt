b3f28a07393c9cbbee36a9cbcc9f25216926f0e7
omer <omer.aynaci@hva.nl>
2023-06-30 13:19:34 +0200

    comments added and user can choose all 3 brushes

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,18 +14,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
-        showBrushSizeChooserDialog()
+        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
+
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
 
 
     }
 
+    /**
+     * @author Ömer Aynaci
+     * shows the brush tickness dialog
+     */
     private fun showBrushSizeChooserDialog() {
         var brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size: ")
         val smallButton = brushDialog.findViewById<View>(R.id.ib_small_brush)
+        val mediumButton = brushDialog.findViewById<View>(R.id.ib_medium_brush)
+        val largeButton = brushDialog.findViewById<View>(R.id.ib_large_brush)
         smallButton.setOnClickListener {
             drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumButton.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        largeButton.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
         brushDialog.show()
